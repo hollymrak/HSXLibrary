6621e75ac83b4e452d866bdcb9f49b00ed07147f
tgyhjudwqgdhyujqwdgyhuwjikqd <inkgamenation@gmail.com>
2026-09-23 06:32:30 +0800

    Update WindUI.lua

--- WindUI.lua
+++ WindUI.lua
@@ -94,7 +94,7 @@ local h=b(game:GetService"HttpService")local i=
 
 d.Heartbeat
 
-local j="https://raw.githubusercontent.com/hollymrak/yeyyeeyeyywyqqyweqwyeqeyqqeyq/refs/heads/main/wqodqdiqidquifqwgjwqifoqjkfof"
+local j="https://raw.githubusercontent.com/hollymrak/HSXLibrary/refs/heads/main/Icons.lua"
 
 local _lOk,l=pcall(function()
 local _src=game.HttpGetAsync and game:HttpGetAsync(j)or h:GetAsync(j)
